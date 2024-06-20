75e788afa02a0e859dd1694ce80a2fb8ee117496
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-20 13:14:21 -0300

    Update footer

--- components/lc/footer/index.vue
+++ components/lc/footer/index.vue
@@ -8,37 +8,44 @@
         <!-- -----------------------------------------------
         First Column
       ----------------------------------------------- -->
-        <v-col cols="12" md="3" sm="6" class="px-xs-0">
+        
+      <!-- <v-col cols="12" md="3" sm="6" class="px-xs-0">
           <h4 class="font-weight-regular font-18">Address</h4>
           <p class="mt-5 text-muted">71 Amsteroum Avenue Cronish Night, NY 35098</p>
-        </v-col>
+        </v-col> -->
+
         <!-- -----------------------------------------------
         Second Column
       ----------------------------------------------- -->
-        <v-col cols="12" md="3" sm="6" class="px-xs-0">
+        
+        <!-- <v-col cols="12" md="3" sm="6" class="px-xs-0">
           <h4 class="font-weight-regular font-18">Phone</h4>
           <p class="mt-5 text-muted">Reception : +205 123 4567</p>
           <p class="text-muted">Office : +207 235 7890</p>
-        </v-col>
+        </v-col> -->
+
         <!-- -----------------------------------------------
         Third Column
       ----------------------------------------------- -->
-        <v-col cols="12" md="3" sm="6" class="px-xs-0">
-          <h4 class="font-weight-regular font-18">Email</h4>
+
+        <!-- <v-col cols="12" md="3" sm="6" class="px-xs-0">
+          <h4 class="font-weight-regular font-18">Contatos</h4>
           <p class="mt-5 text-muted">
-            Office : <a class="dark-link" href="/">info@wrappixel.com</a>
+            E-mail : <a class="dark-link" href="/">yuri.erik.oliveira@gmail.com</a>
           </p>
           <p>
-            <span class="text-muted">Site : </span>
+            <span class="text-muted">Telegram : </span>
             <a class="dark-link" href="https://www.wrappixel.com/"
-              >wrappixel.com</a
+              >@yurimarcon</a
             >
           </p>
-        </v-col>
+        </v-col> -->
+
         <!-- -----------------------------------------------
         Fourth Column
       ----------------------------------------------- -->
-        <v-col cols="12" md="3" sm="6" class="px-xs-0">
+        
+      <!-- <v-col cols="12" md="3" sm="6" class="px-xs-0">
           <h4 class="font-weight-regular font-18">Social</h4>
           <div class="social-icons mt-5">
             <a href="#">
@@ -57,19 +64,19 @@
               <i class="mdi mdi-instagram"></i>
             </a>
           </div>
-        </v-col>
+        </v-col> -->
+        
       </v-row>
       <div class="footer-bottom-bar mt-sm-5 mt-0 font-14 ml-sm-0 ml-n3">
         <div class="d-block d-sm-flex align-center">
           <p class="text-muted mb-sm-0 mb-3">
-            All Rights Reserved by
+            Forked by
             <a href="https://www.wrappixel.com/" class="link">wrappixel.com</a>
           </p>
+
           <div class="ml-auto">
             <div class="d-sm-flex align-center">
-              <a href="/" class="link px-sm-4 ps-0">Terms of Use</a>
-              <a href="/" class="link px-sm-4 px-2">Legal Disclaimer</a>
-              <a href="/" class="link px-sm-4 px-2">Privacy Policy</a>
+              <a href="/" class="link px-sm-4 ps-0">Updated with ♥️ by Yuri Marcon</a>
             </div>
           </div>
         </div>
