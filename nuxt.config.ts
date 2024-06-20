3931fcb41b77e1edcdea96d8e10c64ddc26b68ba
yurimarcon <yurimarconoliveira@gmail.com>
2024-06-20 17:50:34 -0300

    Ajust

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    typescript: {
-        shim: false
-      },
-    build: {
-      //   extractCSS: false,
-        transpile: ["vuetify"],
-      },
-    modules: [
-      '@pinia/nuxt',
-    ],
-    vite: {
-      define: {
-        "process.env.DEBUG": false,
-      },
+  ssr: false,
+  typescript: {
+    shim: false
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    '@pinia/nuxt',
+  ],
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
     },
-    nitro: {
-      serveStatic: true,
-    },
-    devServerHandlers: [],
-    hooks: {
-    },
-    target: 'static',
-    router: {
-      base: '/nuxtjs-uikit/'
-    },
-  
-  })
-
-
-  
+  },
+  nitro: {
+    serveStatic: true,
+  },
+  devServerHandlers: [],
+  hooks: {
+  },
+  target: 'static',
+  router: {
+    base: '/nuxtjs-uikit/'
+  },
+  app: {
+    baseURL: '/nuxtjs-uikit/'
+  }
+})
